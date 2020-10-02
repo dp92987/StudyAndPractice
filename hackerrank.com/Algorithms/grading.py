fe7787794f3afb12298187a08c0ba2edf8ca813b
dp92987 <55208041+dp92987@users.noreply.github.com>
2020-10-02 12:18:00 +0300

    Update hackerrank.com

---
+++ hackerrank.com/Algorithms/grading.py
@@ -0,0 +1,37 @@
+# https://www.hackerrank.com/challenges/grading/problem
+
+import os
+
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+def grading_students(grades):
+    # Write your code here
+    return [grade + 5 - grade % 5 if grade >= 38 and grade % 5 in (3, 4) else grade for grade in grades]
+
+
+def main():
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = grading_students(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
+if __name__ == '__main__':
+    main()
