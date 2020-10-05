608cde8fffd64041c4e605d72c70984c8697b149
dp92987 <55208041+dp92987@users.noreply.github.com>
2020-10-05 20:02:46 +0300

    Update hackerrank/python

---
+++ hackerrank.com/Python/python-string-split-and-join.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+
+def split_and_join(line):
+    # write your code here
+    return '-'.join(line.split())
+
+
+def main():
+    line = input()
+    result = split_and_join(line)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
