f23be25d680d01c2aa19783f8e6ff02ccaab6cc1
dp92987 <55208041+dp92987@users.noreply.github.com>
2020-12-03 23:57:59 +0300

    Update coursera/golang-functions-methods

---
+++ coursera.org/Programming with Google Go/2. Functions, Methods, and Interfaces in Go/week01_01.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := getInput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	numbers, err := getTenNumbersFromInput(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sortBubble(numbers)
+
+	fmt.Println("result:", numbers)
+}
+
+func getInput() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("input: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Trim(input, "\n"), nil
+}
+
+func getTenNumbersFromInput(input string) ([]int, error) {
+	splitInput := strings.Split(input, " ")
+	if len(splitInput) > 10 {
+		splitInput = splitInput[0:10]
+	}
+
+	var numbers = make([]int, len(splitInput))
+	for i, s := range splitInput {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			return []int{}, err
+		}
+		numbers[i] = num
+	}
+
+	return numbers, nil
+}
+
+func sortBubble(numbers []int) {
+	var tmp int
+	var sorted bool
+
+	for {
+		sorted = true
+		for i := 1; i < len(numbers); i++ {
+			if numbers[i] < numbers[i-1] {
+				sorted = false
+				tmp = numbers[i]
+				numbers[i] = numbers[i-1]
+				numbers[i-1] = tmp
+			}
+		}
+		if sorted == true {
+			break
+		}
+	}
+}
